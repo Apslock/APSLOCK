1a14de4c11e1e2ba6e06cca4c90820bedee61821
Jayanth Reddy <jayanthreddy2805@gmail.com>
2026-06-27 10:25:26 +0530

    Apply recent updates

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   compress: true,
-  experimental: {
-    optimizePackageImports: ["lucide-react"],
-  },
   images: {
     remotePatterns: [
       {
